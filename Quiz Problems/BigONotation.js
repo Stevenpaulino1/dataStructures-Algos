32b8820464bd2b6b81af77d744df111120646365
Steven Paulino <stevenpaulino1@hotmail.com>
2019-04-03 13:59:53 -0400

    working

---
+++ Quiz Problems/BigONotation.js
@@ -0,0 +1,6 @@
+// Simplify the big O expression as much as possible - O(n + 10)
+// Simplify the big O expression as much as possible - O(100 * n)
+// Simply the following big O expression as much as possible - O(25)
+// Simply the following big O expression as much as possible - O(n + n + n + n)
+// What you should review
+// Simply the following big O expression as much as possible -  O(n^2 + n^3)
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function addUpTo(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
-  }
-  return total;
-}
+//Write a function that calculates the sum of all numbers from 1 up to and including n
+
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+// console.log(addUpTo(5));
+//
+// function addUpTo(n) {
+//   return (n * (n + 1)) / 2;
+// }
