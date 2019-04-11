ff65e516d8629d2e3b9baf71111ad812f471f065
Steven Paulino <stevenpaulino1@hotmail.com>
2019-04-11 14:13:37 -0400

    Sliding window

---
+++ quizProblems/MultiplePointers/sum-zero-naive.js
@@ -0,0 +1,28 @@
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+// implement a function which accepts a sorted array, and counts the unique values in the array.
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+      // console.log(arr);
+    }
+  }
+  return i + 1;
+}
+
+// console.log(countUniqueValues([1, 2, 3, 3, 4, 4]));
